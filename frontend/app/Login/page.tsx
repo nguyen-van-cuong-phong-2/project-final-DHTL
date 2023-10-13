4ca20e4efb13215c026b6fcae7b10f71d1f5bdb8
cuong <cuongbatbai2001@gmail.com>
2023-10-13 22:18:39 +0700

    .

--- frontend/app/Login/page.tsx
+++ frontend/app/Login/page.tsx
@@ -1,17 +1,38 @@
+import Link from "next/link";
 import Header from "../../components/header/header";
 export default function Login() {
   return (
     <>
-      <div className="w-full flex items-center justify-center h-screen">
-        <div>
-          <h1 className="text-blue-500 text-6xl font-bold ">BlueBook</h1>
-          <span className="font-medium">
-            BlueBook giúp bạn kết nối và chia sẻ với mọi người trong cuộc sống
-            của bạn.
-          </span>
+      <div className="w-full flex items-center h-screen bg-gray-100 gap-10">
+        <div className="ml-60 w-2/5 h-2/3 pt-32 flex-row">
+          <span className="text-6xl text-blue-600 font-bold">bluebook</span>
+          <div>
+            <span className="text-3xl font-normal">
+              {" "}
+              Bluebook giúp bạn kết nối và chia sẻ
+            </span>
+            <br></br>
+            <span className="text-3xl font-normal">
+              với mọi người trong cuộc sống của bạn.
+            </span>
+          </div>
         </div>
-        <div className="bg-slate-500 w-80 h-28">
-
+        <div className=" w-1/3 h-max rounded pt-5 flex-row justify-center bg-white shadow-2xl">
+          <input
+            placeholder="Email hoặc số điện thoại"
+            className="w-11/12 h-16 rounded border-2 ml-6 focus:ring-blue-500 focus:border-blue-500 p-6"
+            required></input>
+          <input type="password"
+            placeholder="Mật khẩu"
+            className="w-11/12 h-16 rounded border-2 ml-6 mt-5 focus:ring-blue-500 focus:border-blue-500 p-6"
+            required></input>
+          <button className="bg-blue-600 w-11/12 ml-6 mt-5 h-16 rounded border-2 text-2xl font-bold text-white mb-5">
+              Đăng nhập
+          </button>
+          <Link href={""} className="text-blue-500 w-10/12 ml-[200px] mt-60 h-16">Quên mật khẩu?</Link>
+          <button className="bg-green-600 w-11/12 ml-6 mt-5 h-16 rounded border-2 text-2xl font-bold text-white mb-5">
+              Tạo tài khoản mới
+          </button>
         </div>
       </div>
     </>
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="h-screen">{children}</body>
+      <body className="h-screen mx-auto ">{children}</body>
     </html>
   );
 }
