0d08977aa4f5a0a34c70e814f08e3015a13e981e
cuong <cuongbatbai2001@gmail.com>
2023-10-13 20:37:19 +0700

    .

---
+++ frontend/app/Login/page.tsx
@@ -0,0 +1,19 @@
+import Header from "../../components/header/header";
+export default function Login() {
+  return (
+    <>
+      <div className="w-full flex items-center justify-center h-screen">
+        <div>
+          <h1 className="text-blue-500 text-6xl font-bold ">BlueBook</h1>
+          <span className="font-medium">
+            BlueBook giúp bạn kết nối và chia sẻ với mọi người trong cuộc sống
+            của bạn.
+          </span>
+        </div>
+        <div className="bg-slate-500 w-80 h-28">
+
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ frontend/components/header/header.tsx
@@ -0,0 +1,11 @@
+export default function Header (){
+  return (
+    <>
+        <div className="w-full h-20 bg-blue-500 flex items-center ">
+            <span className="text-neutral-50 center">
+                BlueBook
+            </span>
+        </div>
+    </>
+  )
+}
