60548bfba064594645612d0b366cf95886e78cfc
cuong <cuongbatbai2001@gmail.com>
2023-11-03 08:08:42 +0700

    .

--- frontend/components/header/header.tsx
+++ frontend/components/header/header.tsx
@@ -1,34 +1,63 @@
 "use client";
-import { AiOutlineMessage, AiOutlineLogout } from "react-icons/ai";
-import { IoMdNotificationsOutline } from "react-icons/io";
-import { FiSettings } from "react-icons/fi";
-import { BiSearchAlt2 } from "react-icons/bi";
-import { PopUpSearch } from "@/components/popup/search";
+
 import { useState } from "react";
+import ChatIcon from "@/public/images/chat.png";
+import NotifiIcon from "@/public/images/notifications.png";
+import avatar from "@/public/images/avatar.jpg";
+import Image from "next/image";
+
 export default function Header() {
   const [popUp, setpopUp] = useState(false);
   return (
     <>
-      <div className="w-full h-24 flex items-center pl-32 justify-between border-2 py-5 pr-5">
-        <span className=" text-5xl font-bold text-blue-500 hover:cursor-pointer">
-          BlueBook
-        </span>
-        <div className="w-1/2 relative">
+      <div
+        className="flex border shadow-md p-3 justify-between
+      "
+      >
+        <div className="flex w-max gap-3">
+          <div className="border rounded-full h-max w-max px-5 py-3 bg-blue-600 box-border">
+            <p className="text-white text-1xl font-bold">B</p>
+          </div>
+
           <input
-            placeholder="Tìm kiếm trên bluebook"
-            className=" text-2xl w-full mt-1 border rounded-3xl py-2.5 bg-gray-100 pl-12 outline-none focus:border-2 h-14"
-            onClick={() => setpopUp(true)}
-          ></input>
-          <BiSearchAlt2 className="absolute top-[22px] left-4 text-2xl text-gray-500"></BiSearchAlt2>
+            placeholder="Tìm kiếm trên BlueBook"
+            className="bg-BGICon w-[300px] border rounded-3xl px-5 outline-none h-[48px]"
+          />
         </div>
-
-        <AiOutlineMessage className="text-4xl mt-3 ml-3 text-slate-500 hover:cursor-pointer"></AiOutlineMessage>
-        <IoMdNotificationsOutline className="text-4xl mt-3 ml-3 text-slate-500 hover:cursor-pointer"></IoMdNotificationsOutline>
-        <FiSettings className="text-4xl mt-3 ml-3 text-slate-500 hover:cursor-pointer"></FiSettings>
-        <div className="text-4xl mt-3 ml-3 text-slate-500 hover:cursor-pointer border rounded-full p-5 bg-red-400"></div>
+        <div className="flex gap-3">
+          <div className="border rounded-full h-max w-max p-3 bg-BGICon">
+            <svg
+              viewBox="0 0 24 24"
+              width="20px"
+              height="20px"
+              fill="currentColor"
+              className="x1lliihq x1k90msu x2h7rmj x1qfuztq x198g3q0 x1qx5ct2 xw4jnvo"
+            >
+              <path d="M.5 12C.5 5.649 5.649.5 12 .5S23.5 5.649 23.5 12 18.351 23.5 12 23.5c-1.922 0-3.736-.472-5.33-1.308a.63.63 0 0 0-.447-.069l-3.4.882a1.5 1.5 0 0 1-1.828-1.829l.882-3.4a.63.63 0 0 0-.07-.445A11.454 11.454 0 0 1 .5 12zm17.56-1.43a.819.819 0 0 0-1.125-1.167L14 11.499l-3.077-2.171a1.5 1.5 0 0 0-2.052.308l-2.93 3.793a.819.819 0 0 0 1.123 1.167L10 12.5l3.076 2.172a1.5 1.5 0 0 0 2.052-.308l2.931-3.793z"></path>
+            </svg>
+          </div>
+          <div className="border rounded-full h-max w-max p-3 bg-BGICon">
+            <svg
+              viewBox="0 0 24 24"
+              width="20px"
+              height="20px"
+              fill="currentColor"
+              className="x1lliihq x1k90msu x2h7rmj x1qfuztq x198g3q0 x1qx5ct2 xw4jnvo"
+            >
+              <path d="M3 9.5a9 9 0 1 1 18 0v2.927c0 1.69.475 3.345 1.37 4.778a1.5 1.5 0 0 1-1.272 2.295h-4.625a4.5 4.5 0 0 1-8.946 0H2.902a1.5 1.5 0 0 1-1.272-2.295A9.01 9.01 0 0 0 3 12.43V9.5zm6.55 10a2.5 2.5 0 0 0 4.9 0h-4.9z"></path>
+            </svg>
+          </div>
+          <Image
+            className="border rounded-full"
+            src={avatar}
+            width={50}
+            height={40}
+            objectFit="cover"
+            quality={100}
+            alt="avatar"
+          />
+        </div>
       </div>
-      {/* {popUp && <PopUpSearch></PopUpSearch>} */}
-
     </>
   );
 }
