cba3242fdf799e58636da72724f184729c0d1915
cuong <cuongbatbai2001@gmail.com>
2023-11-06 08:02:39 +0700

    .

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,16 @@
-import Header from '@/components/header/header';
+import Header from "@/components/header/header";
+import LeftBody from "@/components/Body/left";
+import RightBody from "@/components/Body/right";
+import BettwenBody from "@/components/Body/bettwen";
 export default function Home() {
-  return <>
-  <Header></Header>
-  home
-  </>;
+  return (
+    <>
+      <Header></Header>
+      <div className="flex">
+        <LeftBody />
+        <BettwenBody />
+        <RightBody />
+      </div>
+    </>
+  );
 }
---
+++ frontend/components/Body/right.tsx
@@ -0,0 +1,10 @@
+export default function rightBody() {
+    return (
+      <>
+        <div className=" bg-green-600 w-1/5 h-screen">
+
+        </div>
+      </>
+    );
+  }
+  
